1891dace9219751c01beffcdee19820e52220d14
Matthew Elder <matt@mattelder.org>
2009-06-22 22:08:30 +0000

    added real response headers to test

--- test.hs
+++ test.hs
@@ -5,6 +5,11 @@ import System.IO
 main = do
     service <- listenOn (PortNumber 8000)
     (clientHandle, _, _) <- accept service
+    fileSize <- withFile "test.hs" ReadMode hFileSize
+    hPutStr clientHandle "HTTP/1.1 200 OK\r\n"
+    hPutStr clientHandle "Content-Type: text/plain\r\n"
+    hPutStr clientHandle ("Content-Length: " ++ (show fileSize) ++ "\r\n\r\n")
+
     sendFile clientHandle "test.hs"
     putStrLn ("Sent in mode: " ++ sendFileMode)
     hClose clientHandle
