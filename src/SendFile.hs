680ea7c631dfd6b72a489d784b25e9c9f6981e9e
Matthew Elder <matt@mattelder.org>
2009-06-25 04:26:10 +0000

    merged documentation tweaks

--- src/SendFile.hs
+++ src/SendFile.hs
@@ -8,12 +8,26 @@ module SendFile (
 import qualified SendFile.Internal
 import System.IO (Handle(..))
 
--- | A cross-platform wrapper for sendfile -- this implements an available operating-system call if supported, otherwise it falls back to a portable haskell implementation. It takes a Handle which it will first flush before handing it to the operating system to perform the transmission.
+-- | Get the size of a file.
+fileSize :: FilePath -- ^ The path of the file
+         -> IO Int   -- ^ The size of the file (in bytes)
+fileSize = SendFile.Internal.fileSize
+
+-- | A cross-platform wrapper for sendfile -- this implements an available operating-system call if supported, otherwise it falls back to a portable haskell implementation. It will flush the output Handle before handing it to the operating system for transmission.
 sendFile :: Handle   -- ^ The output Handle
          -> FilePath -- ^ The path where the input file resides
          -> IO ()
 sendFile = SendFile.Internal.sendFile
 
--- | Returns the mode that sendfile was compiled with. Mainly for debugging use. Possible values are 'WIN32_SENDFILE' and 'PORTABLE_SENDFILE'.
+-- | Like sendFile except that it additionally allows you to specify an offset and count. The behavior of reading beyond the end of the file is undefined.
+sendFile' :: Handle   -- ^ The output Handle
+          -> FilePath -- ^ The path where the input file resides
+          -> Int      -- ^ The starting offset of the file (in bytes)
+          -> Int      -- ^ The number of bytes to read
+          -> IO ()
+sendFile' = SendFile.Internal.sendFile'
+
+-- | Returns the mode that sendfile was compiled with. Mainly for debugging use. Possible values are 'WIN32_SENDFILE', 'LINUX_SENDFILE', and 'PORTABLE_SENDFILE'.
 sendFileMode :: String -- ^ The mode that sendfile was compiled with
 sendFileMode = SendFile.Internal.sendFileMode
+
