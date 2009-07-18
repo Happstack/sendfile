7df4cee7fe7c1279c568e987db4f8f9d1428a805
Matthew Elder <matt@mattelder.org>
2009-07-18 01:07:51 +0000

    removed c source files

--- csrc/sendfile_linux.c
+++
@@ -1,9 +0,0 @@
-
-/* LINUX */
-#include <stdlib.h>
-#include <sys/sendfile.h>
-
-ssize_t c_sendfile_linux(const int out_fd, const int in_fd, const size_t count) {
-    return sendfile(out_fd, in_fd, NULL, count);
-}
-
