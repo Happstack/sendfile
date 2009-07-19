95eb12df7add97f83ddc776c75033ed528f9df32
Matthew Elder <matt@mattelder.org>
2009-07-19 18:09:17 +0000

    made freebsd impl return the correct sbytes

--- src/Network/Socket/SendFile/FreeBSD.hs
+++ src/Network/Socket/SendFile/FreeBSD.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 -- | FreeBSD system-dependent code for 'sendfile'.
 module Network.Socket.SendFile.FreeBSD (_sendFile) where
-import Foreign.C.Error (throwErrnoIfMinus1)
+import Foreign.C.Error (throwErrnoIfMinus1_)
 import Foreign.C.Types (CInt, CSize)
+import Foreign.Marshal.Alloc (alloca)
 import Foreign.Ptr (Ptr, nullPtr)
+import Foreign.Storable (peek)
 import System.Posix.Types (COff, Fd)
 
 _sendFile :: Fd -> Fd -> Integer -> IO Integer
-_sendFile out_fd in_fd count =
-    fmap fromIntegral $ throwErrnoIfMinus1
+_sendFile out_fd in_fd count = alloca $ \sbytes -> do
+    throwErrnoIfMinus1_
       "Network.Socket.SendFile.FreeBSD.sendFile'"
-      (c_sendfile_freebsd in_fd out_fd 0 (fromIntegral count) nullPtr nullPtr 0)
+      (c_sendfile_freebsd in_fd out_fd 0 (fromIntegral count) nullPtr sbytes 0)
+    peek sbytes
 
 foreign import ccall unsafe "sys/uio.h sendfile" c_sendfile_freebsd
     :: Fd -> Fd -> COff -> CSize -> Ptr () -> Ptr COff -> CInt -> IO CInt
